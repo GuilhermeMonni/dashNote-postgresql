2c68e66334a05d225a6bd7fb0c10d8c1f59fc91f
Guilherme Monni <gmonni20@gmail.com>
2025-09-23 20:09:47 -0300

    fix infos bancoDados

--- bancoDados.js
+++ bancoDados.js
@@ -2,16 +2,16 @@ import { sql } from "./banco.js";
 import { randomUUID } from "node:crypto";
 
 export class bancoDados {
-  async createUser(username, passHashed, descr) { 
+  async createUser(username, passHashed) { 
     //criação do usuario
     const id = randomUUID(); //id
     const usernameLowercase = username.toLowerCase()
 
-    await sql`insert into users (id, username, password, description) VALUES (${id}, ${usernameLowercase}, ${passHashed}, ${descr})`
+    await sql`insert into user (user_id, user_username, user_password) VALUES (${id}, ${usernameLowercase}, ${passHashed})`
   }
 
   async verUser(username){ //verificar se existe o user
-    const verUser = await sql`select username from users WHERE username = ${username}`
+    const verUser = await sql`select user_username from user WHERE user_username = ${username}`
 
     if(verUser){
         console.log("Usuário encontrado.")
@@ -22,13 +22,13 @@ export class bancoDados {
   }
 
   async loginUser(username){
-    const result = await sql`select * from users where username = ${username}`
+    const result = await sql`select * from user where user_username = ${username}`
 
     return result[0]
   }
 
   async deleteUser(id) { //deletar usuario
-    const delUser = await sql`delete from users where id = ${id}`
+    const delUser = await sql`delete from user where user_id = ${id}`
 
     console.log('Usuário deletado com sucesso!');
   }
