776688104f6905cf18458abe38093d6d4e9ba86d
Guilherme Monni <gmonni20@gmail.com>
2025-10-04 20:15:06 -0300

    fix res addTask

--- server.js
+++ server.js
@@ -117,7 +117,8 @@ server.post('/addTask', async(request, reply) => { //adicionar task
     const userTask = await task.addTasks(task_id, task_userid, task_task, task_date, task_state)
 
    return reply.code(201).send({
-        message: `Sua tarefa foi adicionada com sucesso.`
+        message: `Sua tarefa foi adicionada com sucesso.`,
+        userTask
     }) 
 })
 
